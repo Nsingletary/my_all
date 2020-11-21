0ad6484b48405aa3aaaf260561fdde363950ba37
Nsingletary <singletarybush@gmail.com>
2020-11-20 20:51:53 -0500

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,8 +3,8 @@ require 'pry'
 def my_all?(collection)
     i = 0
     block_return_values = []
-        while i <collectio.length
-            block_return_values << yield
+        while i < collectio.length
+            block_return_values << yield(collection[i])
             i += 1
         end
         if block_return values.include?(false)
